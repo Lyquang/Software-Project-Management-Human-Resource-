4c4192c744dad0628b630553c2bc656eb2ea8137
Bui Quynh <quynh.buiquynh@hcmut.edu.vn>
2025-11-20 15:19:10 +0700

    Fix sth and create AdminDashboard

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,7 @@ import { Provider } from "react-redux";
 // import AdminTraining from "./components/Training/AdminTraining";
 // import Page
 import MainPage from "./Page/MainPage";
+import AdminDashboard from "./Page/AdminDashboard";
 import Home from "./components/Authentication/Home";
 import Login from "./components/Authentication/Login";
 import Signup from "./components/Authentication/Signup";
@@ -46,6 +47,7 @@ root.render(
         <Route path="/signup" element={<Signup />} />
 
         <Route exact path="/login/admin" element={<MainPage />}>
+          <Route index element={<AdminDashboard />}></Route>
           <Route path="employee" element={<AllEmployee />} />
           <Route path="admin-attendance" element={<AdminAttendance />} />
           <Route path="admin-salary" element={<AdminSalary />} />
